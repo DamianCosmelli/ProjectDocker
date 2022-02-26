671f8446d1543ee7f39cc8ce177a750bbe8ac40a
Damian Cosmelli <damiancosmelli@gmail.com>
2022-02-26 15:03:55 -0300

    Delete lasydocker.sh

--- lasydocker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it --name=lazydocker -v \
-/var/run/docker.sock:/var/run/docker.sock \
--v /config_lazydocker:/.config/jesseduffield/lazydocker \
-lazyteam/lazydocker
